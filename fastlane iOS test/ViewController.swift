eaeff61540c18406945b4a17b755b1659edc441a
Mario de Biase <debiasej@gmail.com>
2017-06-11 01:28:55 +0200

    Initial commit

---
+++ fastlane iOS test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fastlane iOS test
+//
+//  Created by Mario de Biase on 10/6/17.
+//  Copyright © 2017 avantgarde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
